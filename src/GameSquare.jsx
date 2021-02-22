56a6b487807b520041ccbad6f462c8f1d98fd559
programmingnate <nsheets97@gmail.com>
2021-02-22 13:43:52 -0800

    Different numbers now appear as different colors

--- src/GameSquare.jsx
+++ src/GameSquare.jsx
@@ -15,7 +15,39 @@ const GameSquare = ({CheckSquare, coords, status}) => {
 
     function RenderNumber(status) {
         if (typeof status === 'number') {
-            return status;
+            var textColor;
+            switch (status) {
+                case 1: 
+                    textColor = 'blue';
+                    break;
+                case 2: 
+                    textColor = 'green';
+                    break;
+                case 3: 
+                textColor = 'red';
+                    break;
+                case 4: 
+                textColor = 'navy';
+                    break;
+                case 5: 
+                textColor = 'maroon';
+                    break;
+                case 6: 
+                textColor = 'black';
+                    break;
+                case 7: 
+                textColor = 'black';
+                    break;
+                case 8: 
+                textColor = 'black';
+                    break;
+            }
+            console.log(textColor, status);
+            return (
+                <div style={{color: textColor}}>
+                    {status}
+                </div>
+            );
         } else {
             return (<div></div>)
         }
